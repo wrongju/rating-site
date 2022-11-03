cadfc1bd3d96b481f9eccab369cd4b0db6aee536
Julia Portseva <104313259+wrongju@users.noreply.github.com>
2022-11-03 17:38:55 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,22 +4,35 @@ let wrapper = document.querySelector('.wrapper-register');
 let thankyou = document.querySelector('.thankyou');
 let span = document.getElementById('rating');
 
-
-for (let i = 0; i < ratings.length; i++) {
-
-	ratings[i].addEventListener('click', () => {
-		let ratingsres = ratings[i].textContent;
-		
-			for (let i = 0; i < ratings.length; i++) {
-			ratings[i].classList.remove('selected');
-			}
-		ratings[i].classList.add('selected');
-		
+ratings.forEach(el => {
+	el.addEventListener('click', () => {
+		let ratingsres = el.textContent;
+		el.classList.remove('selected');
+		el.classList.add('selected');
+	
 		btn.addEventListener('click', () => {
 			wrapper.style.display = 'none'
 			thankyou.style.display = 'block'
 			span.textContent = ratingsres;
 		})
 	})
+})
 
-}
+// OR
+
+// for (let i = 0; i < ratings.length; i++) {
+
+// 	ratings[i].addEventListener('click', () => {
+// 		let ratingsres = ratings[i].textContent;
+
+// 		ratings[i].classList.remove('selected');
+// 		ratings[i].classList.add('selected');
+
+// 		btn.addEventListener('click', () => {
+// 			wrapper.style.display = 'none'
+// 			thankyou.style.display = 'block'
+// 			span.textContent = ratingsres;
+// 		})
+// 	})
+
+// }
